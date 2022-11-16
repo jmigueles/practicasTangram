329bb25551863efb92e7d61d2565e28a521aa33b
Jose <migueless91@gmail.com>
2022-11-16 12:57:44 +0100

    Proyecto finalizado

--- proyectoV2/app_autofirma/applications/certificado/admin.py
+++ proyectoV2/app_autofirma/applications/certificado/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Documento_firmado, Usuario
 
-# Register your models here.
-admin.site.register(Documento_firmado)
-admin.site.register(Usuario)
+''' ---------------------------------------------------
+        PERSONALIZACIÓN DEL MODEL DEL DOCUMENTO
+--------------------------------------------------- '''
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'fecha', 'pdf')
+    search_fields = ('fecha',)
+
+admin.site.register(Documento_firmado, DocumentoAdmin)
+
+''' ---------------------------------------------------
+        PERSONALIZACIÓN DEL MODEL DEL USUARIO
+--------------------------------------------------- '''
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'primer_apellido', 'segundo_apellido', 'dni')
+    search_fields = ('primer_apellido', 'dni')
+
+admin.site.register(Usuario, UsuarioAdmin)
